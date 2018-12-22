Fixed Parzen Window Calculation
Signed-off-by: Mavraj Granit <mavrajgranit@gmail.com>

Layers/ParzenWindow.py
@@ -4,15 +4,16 @@ import torch.nn as nn
 #import plotly
 #import plotly.graph_objs as go
 
-#After reading the "Pattern3.pdf" I think we should learn x and Sigma(?)
+#After reading the "Pattern3.pdf" I think we should learn x and Sigma(?) Update: Weirdly enough i dont have access anymore
 
 #RESOURCES
 #http://www.personal.reading.ac.uk/~sis01xh/teaching/CY2D2/Pattern3.pdf
 #http://www.ehu.eus/ccwintco/uploads/8/89/Borja-Parzen-windows.pdf
 
+
 class ParzenWindow(nn.Module):
 
-    def __init__(self, in_features, out_features, std_init=0.4):
+    def __init__(self, in_features, out_features, std_init=1.0):
         super(ParzenWindow, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -28,13 +29,8 @@ class ParzenWindow(nn.Module):
         self.sigma.data.uniform_(mu_range)
 
     def forward(self, input):
-        '''
-        inp = [input]
-        for i in range(self.out_features-1):
-            inp.append(input)
-        inp = torch.stack(inp)'''
         sum = torch.sum((self.mikro-input) ** 2,1)
-        return torch.exp(-sum)/ (2*self.sigma ** 2)
+        return torch.exp(-sum/ (2*self.sigma ** 2))
 
 '''Beatiful Plots
 pw = ParzenWindow(2,2)

TestPW.py
@@ -6,8 +6,6 @@ import torch.optim as opt
 import plotly
 import plotly.graph_objs as go
 
-#Just tests
-
 #Mini-Batches currently not supported
 x1 = tensor([2,2.5,3,1,6])
 y1= tensor([0.0,1.0])
@@ -15,13 +13,14 @@ y1= tensor([0.0,1.0])
 x2 = tensor([6,6.5,7,3,2])
 y2 = tensor([1.0,0.0])
 
-network = nn.Sequential(nn.Linear(5,5),nn.Sigmoid(),ParzenWindow(5,2))
-optimizer = opt.SGD(network.parameters(),lr=0.005)
+#Can't deal with a lot of not normalized values as inputs
+network = nn.Sequential(nn.Linear(5,5),nn.Linear(5,3),ParzenWindow(3,2))
+optimizer = opt.SGD(network.parameters(),lr=0.001)
 criterion = nn.MSELoss()
 
-for i in range(1000):
-    out1 = network.forward(x1)
-    out2 = network.forward(x2)
+for i in range(5500):
+    out1 = network(x1)
+    out2 = network(x2)
     optimizer.zero_grad()
     loss1 = criterion(out1,y1)
     loss1.backward()
@@ -32,8 +31,8 @@ for i in range(1000):
 print(network(x1))
 print(network(x2))
 
-network = nn.Sequential(nn.Linear(2,5),nn.Sigmoid(),ParzenWindow(5,2))
-optimizer = opt.SGD(network.parameters(),lr=0.005)
+network = nn.Sequential(ParzenWindow(2,3),ParzenWindow(3,2))
+optimizer = opt.SGD(network.parameters(),lr=0.01)
 
 x1 = tensor([-1.0,0.0])
 y1= tensor([0.2,1.0])
@@ -42,10 +41,10 @@ y2= tensor([0.6,0.3])
 x3 = tensor([0.8,-0.4])
 y3= tensor([0.8,1.0])
 
-for i in range(10000):
-    out1 = network.forward(x1)
-    out2 = network.forward(x2)
-    out3 = network.forward(x3)
+for i in range(15000):
+    out1 = network(x1)
+    out2 = network(x2)
+    out3 = network(x3)
     optimizer.zero_grad()
     loss1 = criterion(out1,y1)
     loss1.backward()
@@ -54,7 +53,7 @@ for i in range(10000):
     loss3 = criterion(out3, y3)
     loss3.backward()
     optimizer.step()
-print("new")
+print("Second")
 print(network(x1))
 print(network(x2))
 print(network(x3))
@@ -63,10 +62,10 @@ px = []
 py = []
 pz1 = []
 pz2 = []
-xs = -1.0
-while xs<=1.0:
-    ys = -1.0
-    while ys <= 1.0:
+xs = -2.0
+while xs<=2.0:
+    ys = -2.0
+    while ys <= 2.0:
         out = network(torch.tensor([xs,ys])).data.numpy()
         pz1.append(out[0])
         pz2.append(out[1])